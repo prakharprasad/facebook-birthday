1a8847dde44987c69f88bd0a74691de17db5812a
Prakhar Prasad <prakharpd@gmail.com>
2021-08-10 18:36:16 +0530

    Add comand line switch to cron script

--- backend/cron.py
+++ backend/cron.py
@@ -1,9 +1,30 @@
 import asyncio
+from types import coroutine
+import click
 import core.runner as runner
 from datetime import datetime
+from functools import wraps
 
-async def main():
+def coro(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+
+    return wrapper
+
+@click.command()
+@click.option('--dry-run', '-d', default=True, help='Run this script in dry run mode.')
+@coro
+async def main(dry_run):
         date = datetime.today()
-        output = await runner.execute(date=date, dry_run=True)
+        output = None
+        if dry_run:
+            print("[+] Dry Run")
+            output = await runner.execute(date=date, dry_run=True)
+        else:
+            print("[+] Normal Run")
+            output = await runner.execute(date=date, dry_run=False)
+
         print(output)
-asyncio.run(main())
+if __name__ == '__main__':
+    main()
