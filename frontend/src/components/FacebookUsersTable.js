47f0f3ddfc73121c8ff398f125aae1492f61034f
Prakhar Prasad <prakharpd@gmail.com>
2021-08-11 20:08:23 +0530

    add profile picture instead of uid in user table

--- frontend/src/components/FacebookUsersTable.js
+++ frontend/src/components/FacebookUsersTable.js
@@ -1,10 +1,10 @@
-import { Select, Stack, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import { fetchPUT, successToast } from 'components/Utils';
+import { Avatar, Center, Select, Stack, Table, Tbody, Td, Th, Thead, Tr, Wrap, WrapItem } from '@chakra-ui/react';
+import { fetchPUT, handleErrors, successToast } from 'components/Utils';
 import React from 'react';
-import { handleErrors } from 'components/Utils';
 
-const BlacklistFlags = { true: "True", false: "False" };
-
+//This is a client-side token and safe to share with others
+const facebookClientToken = "243995607358295|9640135797ff857097dd91a504a142f2";
+const blacklistFlags = { true: "True", false: "False" };
 const handleBlacklistChange = (user, selected) => {
     selected = selected === "true";
     fetch(`/api/update_blacklist`, {
@@ -24,14 +24,23 @@ const Trow = ({ children }) => {
         return (children.map((user) => {
             return (
                 <Tr key={user.uid}>
-                    <Td isNumeric>{user.uid}</Td>
-                    <Td>{user.name}</Td>
+                    <Td><Wrap>
+                        <Center>
+                            <WrapItem>
+                                <Avatar name={user.name} src={`https://graph.facebook.com/v11.0/${user.uid}/picture?access_token=${facebookClientToken}`} mr={2} />
+                            </WrapItem>
+                            <WrapItem>
+                                {user.name}
+                            </WrapItem>
+                        </Center>
+                    </Wrap>
+                    </Td>
                     <Td>{user.birthday}</Td>
                     <Td>
                         <Stack spacing={3}>
                             <Select onChange={(e) => handleBlacklistChange(user, e.target.value)} defaultValue={String(user.is_blacklisted)}>
-                                <option value={String(user.is_blacklisted)}>{BlacklistFlags[user.is_blacklisted]}</option>
-                                <option value={String(!user.is_blacklisted)}>{BlacklistFlags[!user.is_blacklisted]}</option>
+                                <option value={String(user.is_blacklisted)}>{blacklistFlags[user.is_blacklisted]}</option>
+                                <option value={String(!user.is_blacklisted)}>{blacklistFlags[!user.is_blacklisted]}</option>
                             </Select>
                         </Stack>
                     </Td>
@@ -48,7 +57,6 @@ export default function FacebookUsersTable({ children }) {
             <Table>
                 <Thead>
                     <Tr>
-                        <Th>UID</Th>
                         <Th>Name</Th>
                         <Th>Birthday</Th>
                         <Th>Blacklisted</Th>
