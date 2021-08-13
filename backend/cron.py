cd4dd01bbd6fd102a771acadc032362343b87544
prakharprasad <prakharpd@gmail.com>
2021-08-13 16:42:39 +0530

    add delay in cron script

--- backend/cron.py
+++ backend/cron.py
@@ -3,7 +3,10 @@ from types import coroutine
 import click
 import core.runner as runner
 from datetime import datetime
+from random import randint
+from time import sleep
 from functools import wraps
+
 
 def coro(f):
     @wraps(f)
@@ -12,19 +15,24 @@ def coro(f):
 
     return wrapper
 
+
 @click.command()
 @click.option('--dry-run', '-d', default=True, help='Run this script in dry run mode.')
 @coro
 async def main(dry_run):
-        date = datetime.today()
-        output = None
-        if dry_run:
-            print("[+] Dry Run")
-            output = await runner.execute(date=date, dry_run=True)
-        else:
-            print("[+] Normal Run")
-            output = await runner.execute(date=date, dry_run=False)
+    date = datetime.today()
+    output = None
+    # Wait between 10 to 25 minutes before executing the runner for actual runs
+    sleep_duration = randint(10, 25)
+    print(f"[+] Sleeping for {sleep_duration} minutes")
+    if dry_run:
+        print("[+] Dry Run")
+        output = await runner.execute(date=date, dry_run=True)
+    else:
+        sleep(sleep_duration * 60)
+        print("[+] Normal Run")
+        output = await runner.execute(date=date, dry_run=False)
 
-        print(output)
+    print(output)
 if __name__ == '__main__':
     main()
